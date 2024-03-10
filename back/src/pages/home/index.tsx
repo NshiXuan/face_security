7543157cf98187e2b2de2eb09670d92472329c28
codersx <1743091664@qq.com>
2024-03-10 13:47:36 +0800

    feat: finish echart show by dynamic data

--- back/src/pages/home/index.tsx
+++ back/src/pages/home/index.tsx
@@ -1,49 +1,21 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 import * as echarts from 'echarts';
+import { getUserList } from '@/service/user';
+import { TestUserList } from '@/data/user-data';
+import { IUser } from '@/type';
 
 type EChartsOption = echarts.EChartsOption;
 
-const Home = function () {
-  const cDom1 = useRef(null);
-  const cDom2 = useRef(null);
-  const cInstance1 = useRef<any>();
-  const cInstance2 = useRef<any>();
-  const option1: EChartsOption = {
-    title: {
-      text: '离开或进入小区人数',
-      left: 'center'
-    },
-    tooltip: {
-      trigger: 'item'
-    },
-    legend: {
-      orient: 'vertical',
-      left: 'left'
-    },
-    series: [
-      {
-        name: 'Access From',
-        type: 'pie',
-        radius: '50%',
-        data: [
-          { value: 1048, name: '离开小区人数' },
-          { value: 735, name: '进入小区人数' },
-        ],
-        emphasis: {
-          itemStyle: {
-            shadowBlur: 10,
-            shadowOffsetX: 0,
-            shadowColor: 'rgba(0, 0, 0, 0.5)'
-          }
-        }
-      }
-    ]
-  };
+export type SeriesData = {
+  value: number
+  name: string
+}
 
-  const option2: EChartsOption = {
+function getOption(titleText: string, data: SeriesData[]): EChartsOption {
+  return {
     title: {
-      text: '小区人数与陌生人人数',
+      text: titleText,
       left: 'center'
     },
     tooltip: {
@@ -58,10 +30,7 @@ const Home = function () {
         name: 'Access From',
         type: 'pie',
         radius: '50%',
-        data: [
-          { value: 1048, name: '小区人数' },
-          { value: 100, name: '陌生人人数' },
-        ],
+        data: data as any,
         emphasis: {
           itemStyle: {
             shadowBlur: 10,
@@ -72,26 +41,55 @@ const Home = function () {
       }
     ]
   }
+}
+
+const Home = function () {
+  const cDom1 = useRef(null);
+  const cDom2 = useRef(null);
+  const cInstance1 = useRef<any>();
+  const cInstance2 = useRef<any>();
+
   useEffect(() => {
-    if (cDom1.current) {
-      // 校验 Dom 节点上是否已经挂载了 ECharts 实例，只有未挂载时才初始化
-      cInstance1.current = echarts.getInstanceByDom(cDom1.current);
-      cInstance2.current = echarts.getInstanceByDom(cDom2.current!);
-      const initChart = (instances: any[], doms: any[], options: any[]) => {
-        for (let i = 0; i < instances.length; i++) {
-          const instance = instances[i];
-          const dom = doms[i];
-          const option = options[i];
-          if (!instance.current) {
-            instance.current = echarts.init(dom.current, null, {
-              renderer: 'svg',
-            });
-          }
-          instance.current.setOption(option);
-        }
+    let allUsers: IUser[] = []
+    getUserList().then(res => {
+      if (res.data) {
+        allUsers = [...TestUserList, ...res.data]
+      } else {
+        allUsers = [...TestUserList]
       }
-      initChart([cInstance1, cInstance2], [cDom1, cDom2], [option1, option2])
-    }
+
+      if (cDom1.current) {
+        // 校验 Dom 节点上是否已经挂载了 ECharts 实例，只有未挂载时才初始化
+        cInstance1.current = echarts.getInstanceByDom(cDom1.current);
+        cInstance2.current = echarts.getInstanceByDom(cDom2.current!);
+        const initChart = (instances: any[], doms: any[], options: any[]) => {
+          for (let i = 0; i < instances.length; i++) {
+            const instance = instances[i];
+            const dom = doms[i];
+            const option = options[i];
+            if (!instance.current) {
+              instance.current = echarts.init(dom.current, null, {
+                renderer: 'svg',
+              });
+            }
+            instance.current.setOption(option);
+          }
+        }
+
+        const inUsers = allUsers.filter(item => item.is_login == 2)
+        const outUsers = allUsers.filter(item => item.is_login == 1)
+        initChart([cInstance1, cInstance2], [cDom1, cDom2], [
+          getOption('离开或进入小区人数', [
+            { value: inUsers.length, name: `离开小区人数（${inUsers.length}人）` },
+            { value: outUsers.length, name: `进入小区人数（${outUsers.length}人）` },
+          ]),
+          getOption('离开或进入小区人数', [
+            { value: inUsers.length, name: `离开小区人数（${inUsers.length}人）` },
+            { value: outUsers.length, name: `进入小区人数（${outUsers.length}人）` },
+          ])
+        ])
+      }
+    })
 
     return () => {
       // 容器被销毁之后，销毁实例，避免内存泄漏
